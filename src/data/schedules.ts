8131dcd00995652e802b06c363a58450a5eee326
felipenino13 <cristianfelipenino@gmail.com>
2026-08-13 09:14:54 -0500

    ajuste en el horario de de entrada y salida de entrabajo tambien con el ajuste de la hora a la que salgo de la oficina

--- src/data/schedules.ts
+++ src/data/schedules.ts
@@ -40,30 +40,30 @@ const lateOfficeEvening: ScheduleEntry[] = [
 
 function createOfficeEntries(endsAtFive = false): ScheduleEntry[] {
   return [
-    { time: "5:00 a. m.", duration: "15m", activity: "Despertar" },
-    { time: "5:15 a. m.", duration: "15m", activity: "Baño" },
-    { time: "5:30 a. m.", duration: "30m", activity: "Desayuno" },
-    { time: "6:00 a. m.", duration: "2h", activity: "Desplazamiento al trabajo" },
+    { time: "4:00 a. m.", duration: "15m", activity: "Despertar" },
+    { time: "4:15 a. m.", duration: "15m", activity: "Baño" },
+    { time: "4:30 a. m.", duration: "30m", activity: "Desayuno" },
+    { time: "5:00 a. m.", duration: "2h", activity: "Desplazamiento al trabajo" },
     ...(endsAtFive ? workdayUntilFive : workdayUntilSix),
     { time: endsAtFive ? "5:00 p. m." : "6:00 p. m.", duration: "2h", activity: "Desplazamiento a casa" },
     { time: endsAtFive ? "7:00 p. m." : "8:00 p. m.", duration: "30m", activity: "Acomodarse en casa" },
     ...(endsAtFive ? evening : lateOfficeEvening),
-    { time: "10:30 p. m.", duration: "6h 30m", activity: "Dormir" },
+    { time: "10:30 p. m.", duration: "5h 30m", activity: "Dormir" },
   ];
 }
 
 function createHomeEntries(endsAtFive = false): ScheduleEntry[] {
   return [
-    { time: "5:00 a. m.", duration: "15m", activity: "Despertar" },
-    { time: "5:15 a. m.", duration: "15m", activity: "Baño" },
-    { time: "5:30 a. m.", duration: "1h 30m", activity: "Estudio" },
-    { time: "7:00 a. m.", duration: "30m", activity: "Desayuno" },
-    { time: "7:30 a. m.", duration: "30m", activity: "Ordenar casa" },
+    { time: "4:00 a. m.", duration: "15m", activity: "Despertar" },
+    { time: "4:15 a. m.", duration: "15m", activity: "Baño" },
+    { time: "4:30 a. m.", duration: "1h 30m", activity: "Estudio" },
+    { time: "6:00 a. m.", duration: "30m", activity: "Desayuno" },
+    { time: "6:30 a. m.", duration: "30m", activity: "Ordenar casa" },
     ...(endsAtFive ? workdayUntilFive : workdayUntilSix),
     { time: endsAtFive ? "5:00 p. m." : "6:00 p. m.", duration: endsAtFive ? "2h" : "1h", activity: "Tiempo en familia" },
     { time: "7:00 p. m.", duration: "30m", activity: "Tiempo Cris" },
     ...evening,
-    { time: "10:30 p. m.", duration: "6h 30m", activity: "Dormir" },
+    { time: "10:30 p. m.", duration: "5h 30m", activity: "Dormir" },
   ];
 }
 
